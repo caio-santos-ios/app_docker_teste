c500db69fe95c315adb2be383364d503c1d43d87
caio-santos-ios <caiosantos.dev@outlook.com>
2024-03-24 21:55:11 -0300

    initial commmit

---
+++ src/main/java/br/com/app_docker/app_docker_teste/AppDockerTesteApplication.java
@@ -0,0 +1,13 @@
+package br.com.app_docker.app_docker_teste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppDockerTesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppDockerTesteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/app_docker/app_docker_teste/AppDockerTesteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.app_docker.app_docker_teste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppDockerTesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
